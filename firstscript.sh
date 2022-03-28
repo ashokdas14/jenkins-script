8e2a700f654e948e06dd9f35997a5ac6354ca061
ashokdas14 <sad.kohsa@gmail.com>
2022-03-28 13:45:30 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 gcloud compute instances list
 gcloud compute instances start gcp-test --zone=us-west1-b
+gcloud compute ssh gcp-jenkins --zone=us-west1-b
+sudo apt update
+sudo apt install nginx
+sudo ufw app list
