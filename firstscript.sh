e9d0233e2561027cdc0b9895fd51a6d715a7fc67
ashokdas14 <89927673+ashokdas14@users.noreply.github.com>
2022-03-29 20:27:22 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcloud compute instances list
 gcloud compute instances start gcp-test --zone=us-west1-b
-gcloud compute ssh gcp-jenkins --zone=us-west1-b
+gcloud compute ssh gcp-test --zone=us-west1-b
