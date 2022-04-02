59b764967fbe6f5295d3858addde359dc1f258ee
ashokdas14 <sad.kohsa@gmail.com>
2022-04-02 12:22:14 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -4,6 +4,7 @@ gcloud compute instances start gcp-test --zone=us-west1-b
 gcloud compute ssh gcp-test --zone=us-west1-b
 whoami
 sudo su
+pwd
 whoami
 #sudo apt-get update -y
 #sudo apt-get install nginx -y
