d3ca807682c9138036d4b3653104550618c94b44
ashokdas14 <89927673+ashokdas14@users.noreply.github.com>
2022-04-22 11:24:07 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -3,9 +3,6 @@ gcloud compute instances list
 gcloud compute instances start gcp-nginx --zone=us-west1-a
 ssh jenkins@gcp-nginx
 whoami
-sudo su
-pwd
-whoami
 sudo apt update
 sudo apt install nginx -y
 nginx -v
