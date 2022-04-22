be3a9e39cffcc731f5d06f4f4d34742545b15fb4
ashokdas14 <89927673+ashokdas14@users.noreply.github.com>
2022-04-22 11:07:56 +0530

    Update firstscript.sh

--- firstscript.sh
+++ firstscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 gcloud compute instances list
 gcloud compute instances start gcp-nginx --zone=us-west1-a
-ssh jenkins@gcp-nginx --zone=us-west1-a
+ssh jenkins@gcp-nginx
 whoami
 sudo su
 pwd
